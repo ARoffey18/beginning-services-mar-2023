f7cf3b64cd9d1cee9f8619bf2a444a7c7e04ceb9
Alex Roffey <alexandra_roffey@progressive.com>
2023-03-29 09:40:11 -0400

    wed morning sync

--- backend/LocationsSolution/LocationsApi/Models/StatusModels.cs
+++ backend/LocationsSolution/LocationsApi/Models/StatusModels.cs
@@ -1,18 +1,12 @@
 ﻿namespace LocationsApi.Models;
 
 
-
-
 public class GetStatusResponse
 {
-    public ContactInfo ContactInfo { get; set; } = new();
+    public ContactInfo? ContactInfo { get; set; } = new();
     public Uptime Uptime { get; set; } = new();
 
-
-
 }
-
-
 
 public class ContactInfo
 {
@@ -20,17 +14,13 @@ public class ContactInfo
     public string Phone { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
 
-
-
 }
-
-
 
 public class Uptime
 {
     public int Days { get; set; }
     public int Hours { get; set; }
     public int Minutes { get; set; }
-    public int Seconds { get; set; }    
+    public int Seconds { get; set; }
     public int Milliseconds { get; set; }
 }
